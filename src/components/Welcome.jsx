ea830552d2e3164e084ac36e08fcb5cff6510252
Eddrick Liu <eddrickliu@yahoo.com>
2022-03-10 12:37:07 -0800

    Adding commiting current Mint button

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
-import { AiFillPlayCircle } from "react-icons/ai";
-import { SiEthereum } from "react-icons/si";
-import { BsInfoCircle } from "react-icons/bs";
+import { AiFillPlayCircle, AiOutlineUpload } from 'react-icons/ai';
+import { SiEthereum } from 'react-icons/si';
+import { BsInfoCircle } from 'react-icons/bs';
+import {membersNameAndBios} from '../constants/team'
 
 import { TransactionContext } from "../context/TransactionContext";
 import { Loader } from "./";
@@ -31,8 +32,22 @@ const Welcome = () => {
 
     }
 
+    const handleMint = () => {
+
+    }
+
     return (
-        <div className="flex w-full justify-center items-center">
+        <div className="flex w-full flex-col justify-center items-center">
+            <button
+                type="button"
+                onClick={connectWallet}
+                className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+            >
+                <AiOutlineUpload className="text-white mr-2" />
+                <p className="text-white text-base font-semibold"> 
+                    Mint
+                </p>
+            </button>
             <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
                 <div className=" flex flex-1 justify-start flex-col mf:mr-10">
                     <div>
